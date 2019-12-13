0eca8721dca84d2d3d3c3c4e623af7399cc965f1
fzjzZZ <50373331+fzjzZZ@users.noreply.github.com>
2019-12-13 16:45:16 +0800

    多线程

--- assets/fofa pro/fofa_pro.py
+++ assets/fofa pro/fofa_pro.py
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
-import pyfofa,argparse
-
+import pyfofa,argparse,os,sys
+from threading import Thread
+from time import sleep
 def check_user():
-	'''
-	请将邮箱以及API值填入此处
-	'''
-	email = ''
-	key = ''
-	client = pyfofa.FofaAPI(email,key)
-	userinfo = client.get_userinfo()
-	try:
-		print('[*] success!')
-		print(f'用户名:\t\t{userinfo["username"]}\nvip等级:\t\t{userinfo["vip_level"]}\n剩余F币:\t\t{userinfo["fcoin"]}\n\n')
-		return client
-	except:
-		print(f'[*] error:{userinfo["errmsg"]}')
-		print('[*] exit!')
-		exit()
+    '''
+    请将邮箱以及API值填入此处
+    '''
+    email = ''
+    key = ''
+    client = pyfofa.FofaAPI(email,key)
+    userinfo = client.get_userinfo()
+    try:
+        print('[*] success!')
+        print(f'用户名:\t\t{userinfo["username"]}\nvip等级:\t\t{userinfo["vip_level"]}\n剩余F币:\t\t{userinfo["fcoin"]}\n\n')
+        return client
+    except:
+        print(f'[*] error:{userinfo["errmsg"]}')
+        print('[*] exit!')
+        sys.exit()
+
 def save(res,grammar):
-	with open(f'.//result//{grammar}.txt','w') as f:
-		for i in res:
-			f.write(i+'\n')
-	print(f'[*] info:共收集{len(res)},并保存在.//result//{grammar}.txt中')
-	exit()
+    with open(f'./result/{grammar}.txt','a') as f:
+        for i in res:
+            f.write(i+'\n')
 
 def parse_args():
     """
-    :return:进行参数的解析
+    解析参数
     """
     description = "you should add those parameter"                   
     parser = argparse.ArgumentParser(description=description)        
@@ -38,34 +38,57 @@ def parse_args():
     args = parser.parse_args()  
 
     if args.grammar:                                          
-    	return args
+        return args
     else:
-    	print("usage: fofa_pro.py [-h] [-g GRAMMAR] [-p PAGE] [-f FIRST]")
-    	exit()
+        print("usage: fofa_pro.py [-h] [-g GRAMMAR] [-p PAGE] [-f FIRST]")
+        sys.exit()
 
-def Get_Data(grammar,page,first,client):
-	res = []
-	print("[*] info: 小主稍安勿躁,正在收集中")
-	for i in range(first,page+first):
-		data = client.get_data(grammar,i,"host,ip")
-		if data['error'] == True:
-			print(f'[*] error:{data["errmsg"]}')
-			print('[*] exit!')
-			if res:
-				save(res,grammar)
-		for j in data['results']:
-			if 'https' in j[0]:
-				res.append(j[0])
-			else:
-				res.append('http://' + j[0])
-	save(res,grammar)
 
+def dump(grammar,client,page):
+    res = []
+    for j in range(5):
+        try:
+            data = client.get_data(grammar,page,"host,ip")
+            break
+        except:
+            print(f'[*] error:网络连接不稳定,请稍后再试')
+            sys.exit()
+    if data['error'] == True:
+        print(f'[*] error:{data["errmsg"]}')
+        print('[*] exit!')
+        sys.exit()
+    for j in data['results']:
+        if 'https' in j[0]:
+            res.append(j[0])
+        else:
+            res.append('http://' + j[0])
+    print('██',end = '')
+    save(res,grammar)
+
+
+def Get_Data(grammar,Page,first,client): 
+    folder = os.path.exists('.\\result')
+    if not folder:  
+        os.makedirs('.\\result')  
+
+    with open(f'./result/{grammar}.txt','w') as f:
+        f.write('')
+    print("[*] info: 小主稍安勿躁,正在收集中")
+
+    for page in range(first,Page + first):
+        t = Thread(target = dump,args = (grammar,client,page))
+        t.start()
+    t.join()
+    sleep(5)
+    print()
+    print(f'[*] info:共收集{Page * 100},并保存在./result/{grammar}.txt中')
+        
 def main():
-	args = parse_args()
-	client = check_user()
-	Get_Data(args.grammar,args.page,args.first,client)
-	
+    args = parse_args()
+    client = check_user()
+    Get_Data(args.grammar,args.page,args.first,client)
+    
 
 if __name__ == '__main__':
-	main()
+    main()
 
